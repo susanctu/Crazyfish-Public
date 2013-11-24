a2c3d5ad08d6572484b5becc0abc46a6934c4915
Georges Goetz <geo.goetz@gmail.com>
2013-11-24 15:28:08 -0800

    Updated admin handling of the event list.
    The admin.py file had gone out of sync with the latest models.py file,
    so field names needed to be updated.

--- events/admin.py
+++ events/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
-from events.model import Event, Location, Category
+from events.models import Event, Location, Category
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display('city','zip_code','state_province','country')
-    ordering('zip-code')
-    search_fields('city','zip_code')
+    list_display = ('city','zip_code','state_province','country',)
+    ordering = ('zip_code',)
+    search_fields = ('city','zip_code',)
 
 class EventAdmin(admin.ModelAdmin):
-    list_display('name','category','event_date','event_start_time',
-                 'event_end_time','location','price','website')
-    list_filter('category','event_date')
-    ordering = ('category','-event_date')
-    search_fields = ('category','event_date','location')
+    list_display = ('name','event_start_date','event_start_time',
+                    'event_end_time','event_location','price','website',)
+    list_filter = ('category','event_start_date',)
+    ordering = ('category','-event_start_date',)
+    search_fields = ('category','event_start_date','location',)
 
 # Register the models here.
-admin.register(Category)
-admin.register(Location, LocationAdmin)
+admin.site.register(Category)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(Event, EventAdmin)
