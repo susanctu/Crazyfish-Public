ed468a84381c64aff98539aa352518f9db9a51d7
Georges Goetz <ggoetz@stanford.edu>
2014-01-16 16:04:49 -0800

    Added production settings file.

--- cfsite/settings/prod.py
+++ cfsite/settings/prod.py
@@ -2,10 +2,19 @@
 
 
 from common import *
-from os.path import join
-
+import dj_database_url
 
 ########## DEBUG CONFIGURATION
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 ########## END DEBUG CONFIGURATION
+
+# Parse database configuration from $DATABASE_URL
+DATABASES = dict()
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
