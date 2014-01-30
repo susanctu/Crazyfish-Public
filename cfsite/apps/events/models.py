888ba9da11498a80e381d93eecbf558b07342972
Georges Goetz <ggoetz@stanford.edu>
2014-01-29 17:18:04 -0800

    Increased max description length

--- cfsite/apps/events/models.py
+++ cfsite/apps/events/models.py
@@ -18,7 +18,7 @@ FOOD = 'food & wine'
 
 CF_CATEGORIES = [ART, CLASS, CONF, FAM, MUSIC, MEET, FOOD, SPORT]
 
-MAX_DESCRIPTION_LEN = 500
+MAX_DESCRIPTION_LEN = 10000
 MAX_NAME_LEN = 100
 
 ### Models for the event app here ###
